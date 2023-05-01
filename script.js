dce3485abb010b833ea9426c87cb8a68973507b3
Jay Thornton <jaythornton@Jays-MacBook-Air.local>
2023-05-01 18:42:41 -0400

    moving forecast in css

---
+++ script.js
@@ -0,0 +1,5 @@
+let date = document.querySelector("date");
+let city = document.querySelector("city");
+let temp = document.querySelector("temp");
+let wind = document.querySelector("wind");
+let humidity = document.querySelector("humidity");
